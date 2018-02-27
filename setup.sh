f69f6424e7994fe5d4ed09a8bd1cd6ee4d5e81c6
Xavier Oliver-Duocastella <xavier@snap40.com>
2018-02-27 17:51:11 +0000

    Write proper symlinks to avoid catastrophe

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #env sh
 
 echo "Creating symlinks to config files"
-ln -sf bashrc ~/.bashrc
-ln -sf Brewfile ~/Brewfile
-ln -sf zshrc ~/.zshrc
-ln -sf vimrc ~/.vimrc
-ln -sf tmux.conf ~/.tmux.conf
-ln -sf gitconfig ~/.gitconfig
-mkdir -P ~/.config/karabiner/
-ln -sf karabiner.json ~/.config/karabiner/karabiner.json
+ln -sf `pwd`/bashrc ~/.bashrc
+ln -sf `pwd`/Brewfile ~/Brewfile
+ln -sf `pwd`/zshrc ~/.zshrc
+ln -sf `pwd`/vimrc ~/.vimrc
+ln -sf `pwd`/tmux.conf ~/.tmux.conf
+ln -sf `pwd`/gitconfig ~/.gitconfig
+mkdir -p ~/.config/karabiner/
+ln -sf `pwd`/karabiner.json ~/.config/karabiner/karabiner.json
