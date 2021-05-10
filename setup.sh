c91c9f0caac5b9c208cae26b9527e0ff92b1e0aa
Xavier Oliver-Duocastella <xavier@currenthealth.com>
2021-05-10 14:06:18 +0100

    Extra deps and paths

--- setup.sh
+++ setup.sh
@@ -9,3 +9,19 @@ ln -sf `pwd`/tmux.conf ~/.tmux.conf
 ln -sf `pwd`/gitconfig ~/.gitconfig
 mkdir -p ~/.config/karabiner/
 ln -sf `pwd`/karabiner.json ~/.config/karabiner/karabiner.json
+
+
+# Folders and configs, mostly under application support
+# viscosity
+# .ssh
+# .aws (TODO)
+# .pip
+# .m2 (at least config)
+# slack
+# iterm2 (didn't work)
+# shiftit (didn't work)
+# datagrip, idea, pycharm
+# authy
+
+
+# Catalan keyboard for Mac: https://github.com/nadalsol/catalan-keyboard-layout-mac
